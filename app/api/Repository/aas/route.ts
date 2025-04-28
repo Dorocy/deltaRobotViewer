c57cb446d50443fcadec0796924914c1fda84c4a
Dorocy <52811749+Dorocy@users.noreply.github.com>
2025-04-28 15:50:48 +0900

    Update api server Address
    https://106.254.248.74:36941/api/v3.0/shells

--- app/api/Repository/aas/route.ts
+++ app/api/Repository/aas/route.ts
@@ -1,6 +1,6 @@
 export async function GET(req: Request, res: Response) {
   const data = await fetch(
-    "https://hm2024.wefactory.kr/api/v3.0/shells/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vNjA0MF82MDUwXzQwNDJfNTIwMQ==",
+    "https://106.254.248.74:36941/api/v3.0/shells/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vNjA0MF82MDUwXzQwNDJfNTIwMQ==",
     {
       headers: {
         "Content-Type": "application/json",
